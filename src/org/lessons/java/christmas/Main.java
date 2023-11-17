8bd775dc3fdf78c435f8acb3bfcddab43472e23a
patrictobola <tobola92@gmail.com>
2023-11-17 12:38:52 +0100

    done exercise 1

--- src/org/lessons/java/christmas/Main.java
+++ src/org/lessons/java/christmas/Main.java
@@ -1,10 +1,32 @@
 package org.lessons.java.christmas;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		
+		ArrayList<String> regali = new ArrayList<>();
+		String value = null;
+		System.out.print("[0 -> exit] Dimmi il regalo che vuoi aggiungere alla tua lista: ");
+		do {
+			value = in.nextLine();
+			
+			if(value.equals("0")) break;
+			
+			regali.add(value);
+			System.out.println("La lista ha all'interno N." + regali.size() + " di regali");
+			System.out.print("[0 -> exit] Aggiungi un altro regalo: ");
+			
+		} while(!value.equals("0"));
+			
+		
+		for(int x = 0; x < regali.size(); x++) {
+			String regalo = regali.get(x);
+			System.out.println("Regalo N." + (x + 1) + ": " + regalo);
+		}
 	}
 
 }
